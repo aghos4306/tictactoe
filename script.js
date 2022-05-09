6a301baacc196869c2d63a71f8123ec5d3f23896
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2022-05-10 00:25:55 +0300

    created starter project structure

---
+++ script.js
@@ -0,0 +1,18 @@
+/**
+ * Git Flow Process.
+ * The major commands used in git are 
+ * 1. git init: To initialize an empty git repository in your project
+ * 2. git status: To see the current status of files that needs to be staged
+ * 3. git add: This is used to stage file or files ready for commit
+ * 4. git add <filename>: This is used to add specific file name ready for commit
+ * 5. git add . : This is used to add all staged files and make them ready for commit
+ * 6. git commit -m "commit message goes here": This is used to stage added files before pushing to remote repository
+ * 7. git push: This is used to push commits to remote repository.
+ * 
+ * Local repository and remote repository. 
+ * Branching in Git: 
+ * 1. The base branch is typically called the master/main branch.
+ * 2. Each and every other developer will define specific branch usually using naming convention
+ * that is applicable to the Feature being developed. 
+ * 
+ */
